ed115930dd6924c070a4c5c3721cd0651b017d3b
Amritpreet Singh <amritpreetsingh012@gmail.com>
2024-11-18 21:57:45 -0500

    Started making home page

---
+++ script.js
@@ -0,0 +1,10 @@
+// document.addEventListener("mousemove", (e)=>{
+//     const viewportWidth = window.innerWidth;
+//     const viewportHeight = window.innerHeight;
+//     let mouseX = e.clientX
+//     let mouseY = e.clientY
+//     document.querySelector(".chaser").style.opacity = 1
+//     document.querySelector(".chaser").style.transform = `translate(${mouseX}px, ${mouseY}px)`
+// })
+   
+    
