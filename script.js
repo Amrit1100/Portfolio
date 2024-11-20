820fef03b9a0d9249294716f83d1fcc3a1dcee84
Amritpreet Singh <amritpreetsingh012@gmail.com>
2024-11-19 22:05:11 -0500

    Added more sections to home page

--- script.js
+++ script.js
@@ -7,15 +7,25 @@
 //     document.querySelector(".chaser").style.transform = `translate(${mouseX}px, ${mouseY}px)`
 // })
    
-let elementOne = document.querySelector(".hoverme")
+// let elementOne = document.querySelector(".hoverme")
 
-let intervalOne = setInterval(()=>{
-    elementOne.classList.toggle("show")
-}, 1000)
+// let intervalOne = setInterval(()=>{
+//     elementOne.classList.toggle("show")
+// }, 1000)
 
-let elementTwo = document.querySelector(".myimage")
+// let elementTwo = document.querySelector(".myimage")
 
-elementTwo.addEventListener("mouseenter", ()=>{
-    elementOne.classList.remove("show")
-    clearInterval(intervalOne)
+// elementTwo.addEventListener("mouseenter", ()=>{
+//     elementOne.classList.remove("show")
+//     clearInterval(intervalOne)
+// })
+
+document.querySelector(".plusone").addEventListener("click", ()=>{
+    document.querySelector(".wholecontone").classList.toggle("modifiedwholecont")
+    document.querySelector(".contentone").classList.toggle("showcontent")
+})
+
+document.querySelector(".plustwo").addEventListener("click", ()=>{
+    document.querySelector(".wholeconttwo").classList.toggle("modifiedwholecont")
+    document.querySelector(".contenttwo").classList.toggle("showcontent")
 })
