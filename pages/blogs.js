6c6c0a004c1c9766e7a7d79263f31b7746634e34
Amritpreet Singh <amritpreetsingh012@gmail.com>
2025-01-14 03:25:38 -0500

    Updated blogs page

--- pages/blogs.js
+++ pages/blogs.js
@@ -32,9 +32,10 @@ const Blogs = () => {
    {loading && <div className='fixed inset-0 flex justify-center items-center bg-opacity-50r'><div className='text-white text-2xl font-semibold animate-pulse'>Loading...</div></div>}
    {!loading &&  <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 w-[80vw] mx-auto mt-6 gap-10">
     {blogs.map(e=>{
-       return <Link href={`/blogpost/${e.slug}`} className='cursor-pointer'>
-      <div className='rounded-lg overflow-hidden p-4 transition-all'>
+       return <Link href={`/blogpost/${e.slug}`} className='cursor-pointer transition-all hover:scale-105'>
+      <div className=' overflow-hidden transition-all bg-[#070606] shadow-lg shadow-[gray]'>
         <div className='w-full h-[250px] text-center mx-auto'><img src={e.img} alt="" className='w-full h-full object-cover'/></div> 
+        <div className='m-6'>
         <h1 className='mt-3 text-lg font-bold'>{e.title}</h1> 
           <div className="flex my-3 items-center gap-4">
               <div className='flex gap-2 items-center'>
@@ -43,7 +44,8 @@ const Blogs = () => {
               </div>
               <div className='text-gray-600'>{e.date}</div>
           </div>
-          <div className='text-center'><Link href={`/blogpost/${e.slug}`}><Button variant="outline">Read More</Button></Link></div>
+          <div className='text-center'><Link href={`/blogpost/${e.slug}`}><Button variant="outline" className="bg-white text-black hover:scale-105 transition-all">Read More</Button></Link></div>
+          </div>
       </div>
       </Link>
     })}
